1e77ec2f70040e84cb0359f8cd14766c09040fd9
AntonyCheng <1911261716@qq.com>
2024-03-24 15:58:21 +0800

    v1.0.4 更换头像后删除掉COS中的旧头像

--- src/main/java/top/sharehome/springbootinittemplate/service/impl/UserServiceImpl.java
+++ src/main/java/top/sharehome/springbootinittemplate/service/impl/UserServiceImpl.java
@@ -1,14 +1,19 @@
 package top.sharehome.springbootinittemplate.service.impl;
 
+import cn.dev33.satoken.context.SaHolder;
 import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.zookeeper.Login;
 import org.springframework.stereotype.Service;
+import top.sharehome.springbootinittemplate.common.base.Constants;
 import top.sharehome.springbootinittemplate.common.base.ReturnCode;
 import top.sharehome.springbootinittemplate.exception.customize.CustomizeReturnException;
 import top.sharehome.springbootinittemplate.mapper.UserMapper;
 import top.sharehome.springbootinittemplate.model.entity.User;
+import top.sharehome.springbootinittemplate.model.vo.auth.AuthLoginVo;
 import top.sharehome.springbootinittemplate.service.UserService;
+import top.sharehome.springbootinittemplate.utils.oss.tencent.TencentUtils;
 import top.sharehome.springbootinittemplate.utils.satoken.LoginUtils;
 
 import javax.annotation.Resource;
@@ -33,5 +38,8 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
         if (updateResult == 0) {
             throw new CustomizeReturnException(ReturnCode.ERRORS_OCCURRED_IN_THE_DATABASE_SERVICE);
         }
+        AuthLoginVo loginUser = (AuthLoginVo) SaHolder.getStorage().get(Constants.LOGIN_USER_KEY);
+        TencentUtils.delete(loginUser.getAvatar());
+        LoginUtils.syncLoginUser();
     }
 }
